3f2e5444b223654a16f90e409ce8a4e308236e4c
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2024-01-25 10:41:23 -0600

    change on the header is made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Projects from './pages/Projects';
 import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/about' element={<About />}/>
